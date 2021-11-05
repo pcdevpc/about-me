4c37f7d08947e9fdc8fcc08304c53fac4fe6dca8
Chun <hyunmo@amazon.com>
2021-11-05 04:07:29 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7ac1a51b1f88655ab63b.js"
+    "url": "webpack-runtime-5fe46b90124c169d8dcf.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c64901534a11321725a88fdd2f719fe3"
+    "revision": "3d03ec79fbd036e43709f9259d46adc7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "acdf2d0249b36fda32a3785f042c9def"
+    "revision": "70a23d18f48dea24ab01b7b6faeaff98"
   },
   {
     "url": "polyfill-7666a985283b7d65e3c9.js"
